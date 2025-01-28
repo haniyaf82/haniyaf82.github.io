b022a978e2702a73e242ff91a825226351a956d2
Haniya Fatima <haniyaf2021@gmail.com>
2025-01-28 15:11:25 -0600

    Initial setup for Assignment 1

---
+++ web200/assignment1/script.js
@@ -0,0 +1,6 @@
+// This is a external JS file
+console.log("This output is from external script.js file");
+document.write("Hello World! - by document.write");
+alert("This is an alert output: Hello World!");
+
+document.getElementById("output").innerHTML = "Hello World! from external js file - by getElementById ";
